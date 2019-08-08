da98ec79c1670f653863913c44b892b51220a285
hoa phat <hoaphat.nguyenphan@gmail.com>
2019-08-08 11:49:54 +0700

    fix tabs

--- index.js
+++ index.js
@@ -1,77 +1,77 @@
 const _ = require('lodash');
 
 function getDirective(directive, variableValues) {
-	const { name, arguments: args } = directive;
-	const arg = args[0];
-	let directiveValue;
+  const { name, arguments: args } = directive;
+  const arg = args[0];
+  let directiveValue;
 
-	switch (arg.value.kind) {
-		case 'Variable':
-			directiveValue = variableValues[arg.value.name.value];
-			break;
-		case 'BooleanValue':
-			directiveValue = arg.value.value;
-			break;
-		default:
-			return directiveValue;
-	}
+  switch (arg.value.kind) {
+    case 'Variable':
+      directiveValue = variableValues[arg.value.name.value];
+      break;
+    case 'BooleanValue':
+      directiveValue = arg.value.value;
+      break;
+    default:
+      return directiveValue;
+  }
 
-	return {
-		[name.value]: directiveValue
-	};
+  return {
+    [name.value]: directiveValue
+  };
 }
 
 function getFields(field, initial, variableValues) {
-	const { kind, selectionSet } = field;
+  const { kind, selectionSet } = field;
 
-	if (kind === 'Field') {
-		if (selectionSet && selectionSet.selections) {
-			for (let index = 0; index < selectionSet.selections.length; index++) {
-				const selection = selectionSet.selections[index];
-				const { directives = [] } = selection;
+  if (kind === 'Field') {
+    if (selectionSet && selectionSet.selections) {
+      for (let index = 0; index < selectionSet.selections.length; index++) {
+        const selection = selectionSet.selections[index];
+        const { directives = [] } = selection;
 
-				if (directives.length) {
-					for (let i = 0; i < directives.length; i++) {
-						const directive = getDirective(directives[i], variableValues);
+        if (directives.length) {
+          for (let i = 0; i < directives.length; i++) {
+            const directive = getDirective(directives[i], variableValues);
 
-						if (directive) {
-							const { skip, include } = directive;
+            if (directive) {
+              const { skip, include } = directive;
 
-							if (skip === true || include === false) {
-								return initial;
-							}
-						}
-					}
-				}
+              if (skip === true || include === false) {
+                return initial;
+              }
+            }
+          }
+        }
 
-				const resultRecursive = getFields(selection, {}, variableValues);
+        const resultRecursive = getFields(selection, {}, variableValues);
 
-				if (typeof resultRecursive === 'object') {
-					initial = {
-						...initial,
-						[selection.name.value]: {
-							...resultRecursive
-						}
-					};
-				} else {
-					initial = {
-						...initial,
-						[selection.name.value]: resultRecursive
-					};
-				}
-			}
-		}
+        if (typeof resultRecursive === 'object') {
+          initial = {
+            ...initial,
+            [selection.name.value]: {
+              ...resultRecursive
+            }
+          };
+        } else {
+          initial = {
+            ...initial,
+            [selection.name.value]: resultRecursive
+          };
+        }
+      }
+    }
 
-		if (Object.keys(initial).length === 0) {
-			return 1;
-		}
+    if (Object.keys(initial).length === 0) {
+      return 1;
+    }
 
-		return initial;
-	}
+    return initial;
+  }
 }
 
 function deleteFields(fieldNodeMapped, excludeFields) {
-	return _.omit(fieldNodeMapped, excludeFields);
+  return _.omit(fieldNodeMapped, excludeFields);
 }
 
 /**
@@ -84,13 +84,13 @@ function deleteFields(fieldNodeMapped, excludeFields) {
  * @returns
  */
 function getFieldNames(info, options = {}) {
-	const { fieldNodes, variableValues } = info;
-	const { excludeFields = [] } = options;
+  const { fieldNodes, variableValues } = info;
+  const { excludeFields = [] } = options;
 
-	let fieldNodeMapped = getFields(fieldNodes[0], {}, variableValues);
-	fieldNodeMapped = deleteFields(fieldNodeMapped, excludeFields);
+  let fieldNodeMapped = getFields(fieldNodes[0], {}, variableValues);
+  fieldNodeMapped = deleteFields(fieldNodeMapped, excludeFields);
 
-	return fieldNodeMapped;
+  return fieldNodeMapped;
 }
 
 module.exports = getFieldNames;
