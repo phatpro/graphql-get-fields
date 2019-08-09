a31e950d94368ce8d8b2f801d1b979fd7c20c53c
hoa phat <hoaphat.nguyenphan@gmail.com>
2019-08-09 12:44:06 +0700

    remove dependencies, change name func

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ npm install graphql-get-fields
 
 ### Usage
 
+#### with excludeFields
+
 ```sh
 const getFields = require('graphql-get-fields');
 
@@ -36,11 +38,41 @@ const fieldNames = getFields(info, {
 // fieldNames = { userId: 1, id: 1, title: 1, 'details.description': 1 }
 ```
 
+#### with onlyFields
+
+```sh
+const getFields = require('graphql-get-fields');
+
+// Example query
+//      query getTodos {
+//          todos {
+//               userId @include(if: true)
+//               id
+//               title
+//               completed @skip(if: true) @include(if: false)
+//               details {
+//                    description
+//                    createAt
+//                    updateAt
+//               }
+//          }
+//      }
+
+const fieldNames = getFields(info, {
+    excludeFields: ['id'],
+    onlyFields: ['id', 'details', 'details.description']
+})
+
+// Output
+// fieldNames = { id: 1, details: 1, 'details.description': 1 }
+```
+
 ### Params
 
 - info: graphql resolve info object (required)
 - options:
   - excludeFields (default: [])
+  - onlyFields (default: [])
 
 ## License
 
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const _ = require('lodash');
-
 /**
  * - Map directive
  *
@@ -7,7 +5,7 @@ const _ = require('lodash');
  * @param {object} variableValues
  * @returns
  */
-function getDirective(directive, variableValues) {
+function _getDirective(directive, variableValues) {
   const { name, arguments: args } = directive;
   const arg = args[0];
   let directiveValue;
@@ -37,7 +35,7 @@ function getDirective(directive, variableValues) {
  * @param {object} variableValues
  * @returns
  */
-function getFields(field, initial, parentName, variableValues) {
+function _getFields(field, initial, parentName, variableValues) {
   const { kind, selectionSet } = field;
 
   if (kind === 'Field') {
@@ -50,27 +48,24 @@ function getFields(field, initial, parentName, variableValues) {
           let ignore = false;
 
           for (let i = 0; i < directives.length; i++) {
-            const directive = getDirective(directives[i], variableValues);
+            const directive = _getDirective(directives[i], variableValues);
 
             if (directive) {
               const { skip, include } = directive;
               if (skip === true || include === false) {
                 ignore = true;
 
-                // exit current directives loop
                 break;
               }
             }
           }
 
           if (ignore === true) {
-            // move to next selection
-            // not run code below
             continue;
           }
         }
 
-        const resultRecursive = getFields(selection, {}, selection.name.value, variableValues);
+        const resultRecursive = _getFields(selection, {}, selection.name.value, variableValues);
 
         switch (typeof resultRecursive) {
           case 'object':
@@ -107,8 +102,46 @@ function getFields(field, initial, parentName, variableValues) {
  * @param {string[] | string} excludeFields
  * @returns
  */
-function deleteFields(fieldNodeMapped, excludeFields) {
-  return _.omit(fieldNodeMapped, excludeFields);
+function _excludeFields(fieldNodeMapped, excludeFields) {
+  for (let index = 0; index < excludeFields.length; index++) {
+    const excludeField = excludeFields[index];
+
+    fieldNodeMapped[excludeField] && delete fieldNodeMapped[excludeField];
+  }
+
+  return fieldNodeMapped;
+}
+
+/**
+ * - Only fields
+ *
+ * @param {object} fieldNodeMapped
+ * @param {string[]} onlyFields
+ * @returns
+ */
+function _onlyFields(fieldNodeMapped, onlyFields) {
+  const paths = Object.keys(fieldNodeMapped);
+
+  return onlyFields.reduce((prev, curr) => {
+    const value = fieldNodeMapped[curr];
+    if (value) {
+      prev = {
+        ...prev,
+        [curr]: value
+      };
+    } else {
+      const regex = new RegExp(`^${curr}\\.`, 'gs');
+      const isExistPath = paths.find(path => path.match(regex));
+      if (isExistPath) {
+        prev = {
+          ...prev,
+          [curr]: 1
+        };
+      }
+    }
+
+    return prev;
+  }, {});
 }
 
 /**
@@ -118,19 +151,24 @@ function deleteFields(fieldNodeMapped, excludeFields) {
  * @param {object[]} info.fieldNodes
  * @param {object} info.variableValues
  * @param {object} options
+ * @param {string[]} options.onlyFields
  * @param {string[]} options.excludeFields
  * @returns
  */
 function getFieldNames(info, options = {}) {
   const { fieldNodes, variableValues } = info;
-  const { excludeFields = [] } = options;
+  const { onlyFields = [], excludeFields = [] } = options;
 
-  let fieldNodeMapped = getFields(fieldNodes[0], {}, null, variableValues);
-  fieldNodeMapped = deleteFields(fieldNodeMapped, excludeFields);
+  let fieldNodeMapped = _getFields(fieldNodes[0], {}, null, variableValues);
+
+  if (onlyFields.length) {
+    return _onlyFields(fieldNodeMapped, onlyFields);
+  }
+  if (excludeFields.length) {
+    return _excludeFields(fieldNodeMapped, excludeFields);
+  }
 
   return fieldNodeMapped;
 }
 
-module.exports = {
-  getFieldNames
-};
+module.exports = getFieldNames;
